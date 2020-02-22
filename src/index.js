f6e022ca2d057e8e2f7ec68147dc40fc8d1de2fb
Anton <antonlashyk@gmail.com>
2020-02-22 15:16:01 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) {
+      n = n * (-1);
+  }
+  n = n + '';
+  if (n[n.length] === '0') {
+      n.splice(n.length,1);
+  }
+  n = n.split('').reverse().join('');
+  return +n;
 }
+
+
